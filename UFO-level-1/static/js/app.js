2c07eb7f8d8d3b8aea4ee9c76aeb8adb97783867
Skinsella <kinsella.sj@gmail.com>
2019-11-19 18:32:45 -0600

    Finished part one

--- UFO-level-1/static/js/app.js
+++ UFO-level-1/static/js/app.js
@@ -5,6 +5,7 @@ var tableData = data;
 var tbody = d3.select("tbody");
 
 // looping through data set and appending rows using arrow functions
+
 tableData.forEach((ufoSighting) => {
     var row = tbody.append("tr");
     Object.entries(ufoSighting).forEach(([key, value]) => {
@@ -13,11 +14,15 @@ tableData.forEach((ufoSighting) => {
     });
 });
 
+
 // select the button
 var button = d3.select("#filter-btn");
 
 button.on("click", function() {
 // getting input element
+
+    tbody.html("");
+
     var inputElement = d3.select("#datetime");
 
     var inputValue = inputElement.property("value")
@@ -25,7 +30,15 @@ button.on("click", function() {
     console.log(inputValue);
     console.log(tableData);
 
-    var filteredData = tableData.filter(tableData => tableData.date === inputValue);
+    var filteredData = tableData.filter(tableData => tableData.datetime === inputValue);
+
+    filteredData.forEach((filteredData) => {
+        var row = tbody.append("tr");
+        Object.entries(filteredData).forEach(([key, value]) => {
+            var cell = row.append("td");
+            cell.text(value);
+        });
+    });
 
     console.log(filteredData);
 });
